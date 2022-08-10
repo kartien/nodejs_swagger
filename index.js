cb76991d7aa892193bd16761dc3ca5fca06a98dd
kartien <kevinramon523@gmail.com>
2022-08-10 13:06:12 -0500

    Initial config

--- index.js
+++ index.js
@@ -1,18 +1,23 @@
-const express = require("express");
+const express = require('express');
 const cors = require("cors");
 const morgan = require("morgan");
 const low = require("lowdb");
 
-const FileSync = require("lowdb/adapters/FileSync");
-const { application } = require("express");
+const PORT = process.env.PORT || 3000;
+
+const FileSync = require("lowdb/adapter/FileSync");
 
 const adapter = new FileSync("db.json");
-const db = low(adapter)
+const db = low(adapter);
 
-db.defaults({books: []}).write()
+db.defaults({ books: [] }).write();
+
+const app = express();
 
 app.db = db;
 
-app.use(cors())
-
+app.use(cors());
+app.use(express.json());
+app.use(morgan("dev"));
+app.listen(PORT, () => console.log(`The server is running at ${PORT}`));
 
